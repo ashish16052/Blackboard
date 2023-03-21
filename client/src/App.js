5bbcd896b49fd22e1d35b104ecb1e8dd54d27547
ashish <ashishmohite16052@gmail.com>
2023-03-21 10:12:05 +0530

    join room

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
 import TextEditor from './TextEditor/TextEditor';
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
 
 function App() {
   return (
     <div className="App">
-      <TextEditor />
+      <Routes>
+        <Route exact path='/' element={<Navigate to={`/doc/${uuidV4()}`} />} />
+        <Route path='/doc/:id' element={<TextEditor />} />
+      </Routes>
     </div>
   );
 }
--- client/src/TextEditor/TextEditor.js
+++ client/src/TextEditor/TextEditor.js
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 import './TextEditor.scss'
+import { useParams } from 'react-router-dom'
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, false] }, { font: [] }],
@@ -15,15 +17,53 @@ const TOOLBAR_OPTIONS = [
     ['link', 'image', 'blockquote', 'code-block'],
 ]
 
+
 const TextEditor = () => {
-    
+    const { id } = useParams()
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+
+    useEffect(() => {
+    }, [socket, quill, id])
+
+    useEffect(() => {
+        const sock = io("http://localhost:3001", { query: { id } },
+        )
+        sock.emit('join', id)
+        setSocket(sock)
+        return () => {
+            sock.disconnect()
+        }
+    }, [])
+
+    useEffect(() => {
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return
+            socket.emit('send-changes', delta)
+        }
+        quill && quill.on('text-change', handler)
+        return () => {
+            quill && quill.off('text-change', handler)
+        }
+    }, [socket, quill])
+
+    useEffect(() => {
+        const handler = (delta) => {
+            quill && quill.updateContents(delta)
+        }
+        socket && socket.on('recv-changes', handler)
+        return () => {
+            socket && socket.off('recv-changes', handler)
+        }
+    }, [socket, quill])
+
     const wrapperRef = useCallback(wrapper => {
         if (!wrapper) return
-
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } })
+        const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } })
+        setQuill(q)
     }, [])
 
     return (
